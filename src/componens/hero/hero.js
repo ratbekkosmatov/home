93f9a45528c526bb84d84c0b280a809bd8cef898
ratbekkosmatov <111224068+ratbekkosmatov@users.noreply.github.com>
2022-11-13 14:52:49 +0600

    intin (#2)

--- src/componens/hero/hero.js
+++ src/componens/hero/hero.js
@@ -9,9 +9,12 @@ const Hero = () => {
                     <h1>Choose Your Programs,
                         <br/>Pick Your Meal,
                         <br/>Let’s Get Healthy.</h1>
-                    <p>StayHealty provides Online Training, and Consultation About Diet and Exercise tailored based on user needs. We will provide a training model that can be accessed anytime and <br/>anywhere with the best trainers.</p>
+                    <p>StayHealty provides Online Training, and Consultation About Diet and Exercise tailored based <br/>on user needs. We will provide a training model that can be accessed anytime and <br/>anywhere with the best trainers.</p>
                 </div>
-
+                <div className="des">
+                    <button className="btn">Join Now</button>
+                    <button className="btn">Contact Us</button>
+                </div>
                 <div className="hero--youtube">
                     <iframe width="860" height="615" src="https://www.youtube.com/embed/EzB8_STkIQ4"
                             title="YouTube video player" frameBorder="0"
