e9f9e5c41386300934b269e52e9c8cae00178f85
meerim0203 <111224074+meerim0203@users.noreply.github.com>
2022-11-13 14:25:13 +0600

    add sass (#1)
    Co-authored-by: Мээримгул <meerimgul@MacBook-Air-Meerimgul.local>

--- src/componens/pricing/pricing.js
+++ src/componens/pricing/pricing.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Pricing = () => {
     return (
-        <div>
+        <div id="pricing">
+            <div className="container">
+                <div className="pricing--general">
+                    <p>PRICING</p>
+                    <h1>CHOOSE YOUR PACKAGE
+                        <br />FOR WORKOUT</h1>
+                </div>
+
+            </div>
 
         </div>
     );
